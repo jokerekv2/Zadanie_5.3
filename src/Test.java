1535af4988e65fe432ad34b2ed8c399ae2bc248d
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-16 17:35:55 +0200

    rozwiązanie

---
+++ src/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        YearCheck year = new YearCheck();
+        System.out.println(year.isLeap(2012));
+        System.out.println(year.isLeap(2100));
+    }
+}
